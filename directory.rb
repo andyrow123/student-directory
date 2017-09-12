365fc3ca0794c7e5289519400942cd1394cbb401
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-09-12 20:11:31 +0100

    Added save_students method and tested. Added students.csv to .gitignore.

--- directory.rb
+++ directory.rb
@@ -6,10 +6,23 @@ end
 
 @students = []
 
+def save_students
+  # open the file for writing
+  file = File.open('students.csv', 'w')
+  # iterate of the array of students
+  @students. each do |student|
+    student_data = [student[:name], student[:sex], student[:age], student[:cohort]]
+    csv_line = student_data.join(',')
+    file.puts csv_line
+  end
+  file.close
+end
+
 def print_menu
   # 1. print the menu and ask the user what to do
   puts '1. Input the students'
   puts '2. Show the students'
+  puts '3. Save the list to students.csv'
   puts '9. Exit'
 end
 
@@ -25,6 +38,8 @@ def process(selection)
       input_students
     when '2'
       show_students
+    when '3'
+      save_students
     when '9'
       exit # This will cause the program to terminate
     else
